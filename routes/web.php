c1df1c5c84c65bfb879fc53282ae928ccf8fc92b
cdnzn <denzonchristian@gmail.com>
2025-05-28 16:58:31 +0800

    added routes

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,14 @@ use App\Http\Controllers\GamesController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // Redirected to the games page because there is nothing on the welcome page
+    return redirect('/games');
 });
 Route::get('/get-csrf-token', function () {
     return response()->json(['_token' => csrf_token()]);
 });
-Route::get('/', [GamesController::class,])->name('.index');
 // Step 2. make the routes for the rest of the actions
+
+Route::get('/games', [GamesController::class, 'index'])->name('games.index');
+Route::get('/games/{id}', [GamesController::class, 'show'])->name('games.show');
+Route::delete('/games/{id}', [GamesController::class, 'destroy'])->name('games.destroy');
